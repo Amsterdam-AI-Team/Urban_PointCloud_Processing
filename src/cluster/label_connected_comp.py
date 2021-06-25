1997d2ab1539b3b5952357a90595769889f94b41
daanbl <daan.bloembergen@gmail.com>
2021-06-25 17:05:12 +0200

    Some tweaks for the fill_components method (now fill_components_2). Works quite well with octree_level=10, min_points=100, threshold=0.1.

--- src/cluster/label_connected_comp.py
+++ src/cluster/label_connected_comp.py
@@ -8,9 +8,10 @@ import cccorelib
 
 
 class LabelConnectedComp:
-    def __init__(self, octree_level=9):
+    def __init__(self, octree_level=9, min_points=100):
         """ Init variables. """
         self.octree_level = octree_level
+        self.min_points = min_points
 
     def set_input_cloud(self, las, mask):
         """ Function to convert to CloudCompare point cloud. """
@@ -75,6 +76,45 @@ class LabelConnectedComp:
         labels[label_mask] = seed_point_label
 
         print('Filled components with labels based on initial seed points')
+
+        return labels
+
+    def fill_components_2(self, las_label, unlabelled_label, seed_point_label,
+                          mask, threshold=0.1):
+        """ When one initial seed point is found inside a component,
+        make the whole component this label. """
+        label_mask = np.zeros(len(mask), dtype=bool)
+
+        pre_seed_count = np.count_nonzero(las_label == seed_point_label)
+
+        mask_indices = np.where(mask)[0]
+
+        cc_labels, counts = np.unique(self.point_components,
+                                      return_counts=True)
+
+        cc_labels_filtered = cc_labels[counts >= self.min_points]
+
+        for cc in cc_labels_filtered:
+            # select points that belong to the cluster
+            cc_mask = (self.point_components == cc)
+            # cluster size
+            cc_size = np.count_nonzero(cc_mask)
+            if cc_size < self.min_points:
+                continue
+            # number of point in teh cluster that are labelled as seed point
+            seed_count = np.count_nonzero(
+                las_label[mask_indices[cc_mask]] == seed_point_label)
+            # at least X% of the cluster should be seed points
+            if (float(seed_count) / cc_size) > threshold:
+                label_mask[mask_indices[cc_mask]] = True
+
+        # Add label to the regions
+        labels = las_label
+        labels[label_mask] = seed_point_label
+
+        post_seed_count = np.count_nonzero(labels == seed_point_label)
+
+        print(f'{post_seed_count - pre_seed_count} points added.')
 
         return labels
 
