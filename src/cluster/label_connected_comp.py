7c477c2ee3857c1d8cd4522ea9478eb0820e3493
chrise96 <chrise96@hotmail.com>
2021-06-28 11:54:08 +0200

    Region growing (#7)
    * Add first code region growing
    
    * Add clustering based
    
    Co-authored-by: daanbl <daan.bloembergen@gmail.com>

---
+++ src/cluster/label_connected_comp.py
@@ -0,0 +1,94 @@
+import numpy as np
+
+# Two libraries necessary for the CloudCompare Python wrapper
+# Installation instructions in notebook [3. Clustering based region growing]
+import pycc
+import cccorelib
+
+
+class LabelConnectedComp:
+    """
+    Clustering based region growing implementation using label connected comp.
+    """
+    def __init__(self, octree_level=9, min_component_size=100):
+        """ Init variables. """
+        self.octree_level = octree_level
+        self.min_component_size = min_component_size
+
+    def set_input_cloud(self, las, mask):
+        """ Function to convert to CloudCompare point cloud. """
+        # Be aware that CloudCompare stores coordinates on 32 bit floats.
+        # To avoid losing too much precision you should 'shift' your
+        # coordinates if they are 64 bit floats (which is the default in
+        # python land)
+        xs = (las.x[mask] - las.header.x_min).astype(pycc.PointCoordinateType)
+        ys = (las.y[mask] - las.header.y_min).astype(pycc.PointCoordinateType)
+        zs = (las.z[mask] - las.header.z_min).astype(pycc.PointCoordinateType)
+        point_cloud = pycc.ccPointCloud(xs, ys, zs)
+        # Add the global shift to CloudCompare so that it can use it,
+        # for example to display the real coordinates in point picking tool
+        point_cloud.setGlobalShift(-las.header.x_min, -las.header.y_min,
+                                   -las.header.z_min)
+
+        # (Optional) Create (if it does not exists already)
+        # a scalar field where we store the Labels
+        labels_sf_idx = point_cloud.getScalarFieldIndexByName('Labels')
+        if labels_sf_idx == -1:
+            labels_sf_idx = point_cloud.addScalarField('Labels')
+        point_cloud.setCurrentScalarField(labels_sf_idx)
+
+        # You can access the x,y,z fields using self.point_cloud.points()
+
+        self.labels_sf_idx = labels_sf_idx
+        self.point_cloud = point_cloud
+        self.mask = mask
+        self.las_label = las.label
+
+    def label_connected_comp(self):
+        """ Perform Label Connected Components. """
+        component_count = (cccorelib.AutoSegmentationTools
+                           .labelConnectedComponents(self.point_cloud,
+                                                     level=self.octree_level))
+        print('There are {} components found'.format(component_count))
+
+        # Get the scalar field with labels and points coords as numpy array
+        labels_sf = self.point_cloud.getScalarField(self.labels_sf_idx)
+        self.point_components = labels_sf.asArray()
+
+    def fill_components(self, seed_point_label, threshold=0.1):
+        """ When one initial seed point is found inside a component,
+        make the whole component this label. """
+        label_mask = np.zeros(len(self.mask), dtype=bool)
+
+        pre_seed_count = np.count_nonzero(self.las_label == seed_point_label)
+
+        mask_indices = np.where(self.mask)[0]
+
+        cc_labels, counts = np.unique(self.point_components,
+                                      return_counts=True)
+
+        cc_labels_filtered = cc_labels[counts >= self.min_component_size]
+
+        for cc in cc_labels_filtered:
+            # select points that belong to the cluster
+            cc_mask = (self.point_components == cc)
+            # cluster size
+            cc_size = np.count_nonzero(cc_mask)
+            if cc_size < self.min_component_size:
+                continue
+            # number of point in teh cluster that are labelled as seed point
+            seed_count = np.count_nonzero(
+                self.las_label[mask_indices[cc_mask]] == seed_point_label)
+            # at least X% of the cluster should be seed points
+            if (float(seed_count) / cc_size) > threshold:
+                label_mask[mask_indices[cc_mask]] = True
+
+        # Add label to the regions
+        labels = self.las_label
+        labels[label_mask] = seed_point_label
+
+        post_seed_count = np.count_nonzero(labels == seed_point_label)
+
+        print(f'{post_seed_count - pre_seed_count} points added.')
+
+        return labels
---
+++ src/cluster/region_growing.py
@@ -0,0 +1,125 @@
+import numpy as np
+import open3d as o3d
+import copy
+
+from ..utils.math_utils import angle_between
+
+
+class RegionGrowing:
+    """
+    Region growing implementation based on:
+    https://pcl.readthedocs.io/projects/tutorials/en/latest/region_growing_segmentation.html
+    """
+    def __init__(self, threshold_angle=20, threshold_curve=1.0, max_nn=30,
+                 grow_region_knn=15, grow_region_radius=0.2):
+        """ Init variables. """
+        self.threshold_angle = threshold_angle
+        self.threshold_curve = threshold_curve
+
+        self.max_nn = max_nn
+        self.grow_region_knn = grow_region_knn
+        self.grow_region_radius = grow_region_radius
+
+    def set_input_cloud(self, las, seed_point_label, mask):
+        """ Function to convert to o3d point cloud. """
+        coords = np.vstack((las.x[mask], las.y[mask], las.z[mask])).transpose()
+        pcd = o3d.geometry.PointCloud()
+        pcd.points = o3d.utility.Vector3dVector(coords)
+        self.pcd = pcd
+
+        list_of_indices = np.where(las.label[mask] == seed_point_label)[0]
+        if len(list_of_indices) == 0:
+            print('NOTE: Input point cloud does not contain any seed points.')
+        self.list_of_seed_ids = list_of_indices.tolist()
+
+        self.mask_indices = np.where(mask)[0]
+        self.label_mask = np.zeros(len(mask), dtype=bool)
+
+    def _compute_point_curvature(self, coords, pcd_tree, seed_point, method):
+        """ Compute the curvature for a given a cluster of points. """
+        if method == 'radius':
+            _, idx, _ = (pcd_tree.search_radius_vector_3d(
+                         seed_point, self.grow_region_radius))
+        else:
+            _, idx, _ = (pcd_tree.search_knn_vector_3d(
+                         seed_point, self.grow_region_knn))
+
+        neighbors = o3d.utility.Vector3dVector(coords[idx])
+        pcd = o3d.geometry.PointCloud(neighbors)
+        _, cov = pcd.compute_mean_and_covariance()
+        eig_val, _ = np.linalg.eig(cov)
+
+        return (eig_val[0]/(eig_val.sum()))
+
+    def region_growing(self, seed_point_label, las_label, method='knn'):
+        """
+        The work of this region growing algorithm is based on the comparison
+        of the angles between the points normals.
+
+        The same can also be performed in Python using scipy.spatial.cKDTree
+        with query_ball_tree or query.
+        """
+        # Compute the KDTree
+        pcd_tree = o3d.geometry.KDTreeFlann(self.pcd)
+
+        # Compute the normals for each point
+        self.pcd.estimate_normals(search_param=o3d.geometry
+                                  .KDTreeSearchParamHybrid(radius=self.grow_region_radius,
+                                  max_nn=self.max_nn))
+
+        seed_length = len(self.list_of_seed_ids)
+        region = copy.deepcopy(self.list_of_seed_ids)
+
+        # Initialize the indexes of all seed points as processed
+        processed = np.full(len(self.pcd.points), False)
+        processed[self.list_of_seed_ids] = True
+
+        idx = 0
+        while idx < len(self.list_of_seed_ids):
+            seed_point = self.pcd.points[self.list_of_seed_ids[idx]]
+            seed_normal = self.pcd.normals[self.list_of_seed_ids[idx]]
+
+            # For every seed point, the algorithm finds its neighboring points
+            if method == 'radius':
+                k, neighbor_idx, _ = (pcd_tree.search_radius_vector_3d(
+                                      seed_point, self.grow_region_radius))
+            else:
+                k, neighbor_idx, _ = (pcd_tree.search_knn_vector_3d(seed_point,
+                                      self.grow_region_knn))
+
+            # Remove index seed point itself
+            neighbor_idx = neighbor_idx[1:k]
+
+            for neighbor_id in neighbor_idx:
+                # Is this point processed before?
+                if processed[neighbor_id]:
+                    continue
+
+                # Compute angles between two n-dimensional vectors
+                current_angle = angle_between(seed_normal,
+                                              self.pcd.normals[neighbor_id])
+                # The smoothness constraint in degrees
+                if current_angle < self.threshold_angle:
+                    region.append(neighbor_id)
+                    processed[neighbor_id] = True
+
+                    # Compute the curvature for a neighbor_id and its neighbors
+                    curvature = (self._compute_point_curvature(
+                                 np.asarray(self.pcd.points), pcd_tree,
+                                 self.pcd.points[neighbor_id], method))
+
+                    # Result is below threshold, we add it to the seed points
+                    if curvature < self.threshold_curve:
+                        self.list_of_seed_ids.append(neighbor_id)
+
+            idx = idx+1
+
+        print('There are {} points added'.format(len(region) - seed_length))
+
+        self.label_mask[self.mask_indices[region]] = True
+
+        # Add label to the regions
+        labels = las_label
+        labels[self.label_mask] = seed_point_label
+
+        return labels
