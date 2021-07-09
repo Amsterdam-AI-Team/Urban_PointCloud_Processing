46ee29e9c0811ee84884f65fa4d120707d671a54
chrise96 <chrise96@hotmail.com>
2021-07-09 08:25:41 +0200

    Zelfde structuur als bgt_scraper

--- src/scrapers/ndw_scraper.py
+++ src/scrapers/ndw_scraper.py
@@ -5,16 +5,18 @@ import os
 URL = 'https://data.ndw.nu/api/rest/static-road-data/traffic-signs/v1/current-state?'
 
 
-def get_data(town_code, out_folder='', out_file='traffic_signs.csv'):
-    """
-    Scrape traffic sign locations from the NDW API. The town code for
-    Amsterdam is 'GM0363'.
-    """
+def scrape_ndw(town_code='GM0363'):
+    """ Scrape traffic sign locations from the NDW API. """
     base_url = URL + 'town-code={}'.format(town_code)
 
     response = requests.get(base_url)
-    data = response.json()
-    df = pd.json_normalize(data)
+    return response.json()
+
+
+def parse_traffic_signs(json_response, out_folder='',
+                        out_file='traffic_signs.csv'):
+    """ Parse the JSON content and transform it into a table structure. """
+    df = pd.json_normalize(json_response)
 
     df = df.drop_duplicates(['location.rd.x', 'location.rd.y'])
     df = df.rename(columns={'location.rd.x': 'X', 'location.rd.y': 'Y'})
