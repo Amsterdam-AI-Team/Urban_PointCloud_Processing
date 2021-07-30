2c58799a393253f2c99ba3f026a0be1c835d3510
chrise96 <chrise96@hotmail.com>
2021-07-30 13:41:16 +0200

    Noise filter

--- src/fusion/__init__.py
+++ src/fusion/__init__.py
@@ -1,5 +1,7 @@
 from .ahn_fuser import AHNFuser
 from .bgt_fuser import BGTBuildingFuser, BGTPointFuser
 from .car_fuser import CarFuser
+from .noise_filter import NoiseFilter
 
-__all__ = ['AHNFuser', 'BGTBuildingFuser', 'BGTPointFuser', 'CarFuser']
+__all__ = ['AHNFuser', 'BGTBuildingFuser', 'BGTPointFuser', 'CarFuser',
+           'NoiseFilter']
