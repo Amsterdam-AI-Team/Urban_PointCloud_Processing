6fc65e5a73d730caa92da08a116a2f38483b08b7
daanbl <daan.bloembergen@gmail.com>
2021-08-09 18:00:23 +0200

    Improved visualisations and demo notebook.

--- src/utils/plot_utils.py
+++ src/utils/plot_utils.py
@@ -14,7 +14,35 @@ from ..utils.labels import Labels
 from ..utils.interpolation import FastGridInterpolator
 
 
-def plot_cloud_slice(las_file, ahn_reader, plane_height=1.5, hide_noise=False):
+bgt_labels = {'boom': 'Tree',
+              'lichtmast': 'Lamp post',
+              'verkeersbord': 'Traffic sign',
+              'pand': 'Building',
+              'wegdeel': 'Road'}
+bgt_colors = {'boom': 'green',
+              'lichtmast': 'orange',
+              'verkeersbord': 'darkviolet',
+              'pand': 'lightblue',
+              'pand_poly': 'royalblue',
+              'wegdeel': 'lightgrey'}
+cloud_colors = {'Unlabelled': 'lightgrey',
+                'Ground': 'peru',
+                'Building': 'lightblue',
+                'Tree': 'green',
+                'Street light': 'orange',
+                'Traffic sign': 'darkviolet',
+                'Traffic light': 'red',
+                'Car': 'grey',
+                'Noise': 'whitesmoke'}
+
+
+def plot_cloud_slice(las_file, ahn_reader, plane_height=1.5, hide_noise=False,
+                     ax=None, title=None, legend_below=False):
+    full_plot = False
+    if ax is None:
+        fig, ax = plt.subplots(1, constrained_layout=True)
+        full_plot = True
+
     las = las_utils.read_las(las_file)
     labels = las.label
     points = np.vstack((las.x, las.y, las.z)).T
@@ -31,40 +59,48 @@ def plot_cloud_slice(las_file, ahn_reader, plane_height=1.5, hide_noise=False):
                   & (points[:, 2] <= points_z + plane_height + 0.05))
     label_set = np.unique(labels[plane_mask])
 
-    fig, ax = plt.subplots(1, constrained_layout=True)
-
     for label in label_set:
         if label == Labels.NOISE and hide_noise:
             continue
         label_mask = plane_mask & (labels == label)
+        label_str = Labels.get_str(label)
         ax.scatter(points[label_mask, 0], points[label_mask, 1],
-                   marker='.', label=Labels.get_str(label))
+                   c=cloud_colors[label_str], marker='.', label=label_str)
 
     box = patches.Rectangle((x_min, y_min), x_max-x_min, y_max-y_min,
-                            linewidth=1, linestyle='--', edgecolor='grey',
+                            linewidth=1, linestyle='--', edgecolor='black',
                             fill=False)
-
     ax.add_patch(box)
-    ax.set_title(tilecode)
+
+    if title is not None:
+        ax.set_title(title)
     ax.set_xlabel('X')
-    ax.set_ylabel('Y')
     ax.set_xticks(range(x_min, x_max+1, 10))
     ax.set_xticklabels(range(x_min, x_max+1, 10))
+    ax.set_ylabel('Y')
     ax.set_yticks(range(y_min, y_max+1, 10))
     ax.set_yticklabels(range(y_min, y_max+1, 10))
-    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-    plt.show()
+    if not legend_below:
+        ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    else:
+        ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15), ncol=3)
+    ax.set_aspect('equal', adjustable='box')
+    if full_plot:
+        plt.show()
 
 
-def plot_bgt(tilecode, building_file=None, road_file=None, point_file=None):
-    pt_labels = {'boom': 'Tree',
-                 'lichtmast': 'Lamp post',
-                 'verkeersbord': 'Traffic sign'}
-    pt_colors = {'boom': 'green',
-                 'lichtmast': 'orange',
-                 'verkeersbord': 'red'}
-
+def plot_bgt(tilecode, building_file=None, road_file=None, point_file=None,
+             ax=None, title=None, show_legend=True, legend_below=False):
     padding = 2.5
+
+    full_plot = False
+    if ax is None:
+        width = 7 if show_legend else 5
+        fig, ax = plt.subplots(1, figsize=(width, 5), constrained_layout=True)
+        full_plot = True
+
+    if title is None:
+        title = f'BGT tile {tilecode}'
 
     buildings = []
     roads = []
@@ -79,48 +115,56 @@ def plot_bgt(tilecode, building_file=None, road_file=None, point_file=None):
 
     ((x_min, y_max), (x_max, y_min)) =\
         las_utils.get_bbox_from_tile_code(tilecode)
-    fig, ax = plt.subplots(1, figsize=(7, 5), constrained_layout=True)
 
     for poly in [Polygon(bld) for bld in buildings]:
         x, y = poly.exterior.xy
-        ax.fill(x, y, c='lightblue', label='Building')
-        ax.plot(x, y, c='royalblue')
+        ax.fill(x, y, c=bgt_colors['pand'], label=bgt_labels['pand'])
+        ax.plot(x, y, c=bgt_colors['pand_poly'])
 
     for poly in [Polygon(rd) for rd in roads]:
         x, y = poly.exterior.xy
-        ax.fill(x, y, c='lightgrey', label='Road')
+        ax.fill(x, y, c=bgt_colors['wegdeel'], label=bgt_labels['wegdeel'])
 
     for pt in points:
         ax.scatter(pt[1], pt[2],
-                   c=pt_colors[pt[0]], marker='x', label=pt_labels[pt[0]])
+                   c=bgt_colors[pt[0]], marker='x', label=bgt_labels[pt[0]])
 
     box = patches.Rectangle((x_min, y_min), x_max-x_min, y_max-y_min,
                             linewidth=1, linestyle='--', edgecolor='black',
                             fill=False)
     ax.add_patch(box)
 
-    ax.set_title(f'bgt_{tilecode}')
+    ax.set_title(title)
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
 
-    handles, labels = plt.gca().get_legend_handles_labels()
-    by_label = dict(zip(labels, handles))
-    ax.legend(by_label.values(), by_label.keys(),
-              loc='center left', bbox_to_anchor=(1, 0.5))
+    if show_legend:
+        handles, labels = ax.get_legend_handles_labels()
+        by_label = dict(zip(labels, handles))
+        if not legend_below:
+            ax.legend(by_label.values(), by_label.keys(),
+                      loc='center left', bbox_to_anchor=(1, 0.5))
+        else:
+            ax.legend(by_label.values(), by_label.keys(),
+                      loc='upper center', bbox_to_anchor=(0.5, -0.15), ncol=3)
 
     ax.set_xticks(range(x_min, x_max+1, 10))
     ax.set_xticklabels(range(x_min, x_max+1, 10))
     ax.set_yticks(range(y_min, y_max+1, 10))
     ax.set_yticklabels(range(y_min, y_max+1, 10))
 
-    plt.xlim((x_min - padding, x_max + padding))
-    plt.ylim((y_min - padding, y_max + padding))
-    plt.show()
+    ax.set_xlim((x_min - padding, x_max + padding))
+    ax.set_ylim((y_min - padding, y_max + padding))
+    ax.set_aspect('equal', adjustable='box')
+    if full_plot:
+        plt.show()
 
 
-def plot_ahn_surface(ahn_tile, surf='ground_surface', ax=None):
+def plot_ahn_surface(ahn_tile, surf='ground_surface', ax=None, cbar_pad=0.04):
     cmap = {'ground_surface': 'gist_earth',
             'building_surface': 'Oranges'}
+    cbar_title = {'ground_surface': 'Ground elevation (m)',
+                  'building_surface': 'Building height (m)'}
     full_plot = False
     if ax is None:
         full_plot = True
@@ -135,21 +179,35 @@ def plot_ahn_surface(ahn_tile, surf='ground_surface', ax=None):
                    interpolation='none', cmap=cmap[surf])
 
     ax.set_title(surf)
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
     ax.set_xticks(range(x_min, x_max+1, 10))
     ax.set_xticklabels(range(x_min, x_max+1, 10))
     ax.set_yticks(range(y_min, y_max+1, 10))
     ax.set_yticklabels(range(y_min, y_max+1, 10))
-    cbar = plt.colorbar(im, ax=ax, fraction=0.046, pad=0.04)
-    cbar.ax.set_ylabel('Elevation (m)', rotation=270, labelpad=10)
+    cbar = plt.colorbar(im, ax=[ax], fraction=0.046, pad=cbar_pad)
+    cbar.ax.set_ylabel(cbar_title[surf], rotation=270, labelpad=10)
     if full_plot:
         plt.show()
 
 
-def plot_ahn_sidebyside(ahn_tile):
+def plot_ahn_sidebyside(tilecode, ahn_reader):
+    ahn_tile = ahn_reader.filter_tile(tilecode)
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
     plot_ahn_surface(ahn_tile, surf='ground_surface', ax=ax1)
     plot_ahn_surface(ahn_tile, surf='building_surface', ax=ax2)
-    plt.tight_layout()
+    ax2.set_yticklabels([])
+    ax2.yaxis.label.set_visible(False)
+    fig.suptitle(f'AHN tile {tilecode}', fontsize=14)
+    plt.show()
+
+
+def plot_ahn_merged(tilecode, ahn_reader):
+    ahn_tile = ahn_reader.filter_tile(tilecode)
+    fig, ax1 = plt.subplots(1, figsize=(7, 5))
+    plot_ahn_surface(ahn_tile, surf='ground_surface', ax=ax1, cbar_pad=0.14)
+    plot_ahn_surface(ahn_tile, surf='building_surface', ax=ax1, cbar_pad=0.04)
+    ax1.set_title(f'AHN tile {tilecode}', fontsize=14)
     plt.show()
 
 
@@ -176,17 +234,18 @@ def plot_buildings_ahn_bgt(tilecode, ahn_reader, building_file, offset=1):
 
     for poly in [Polygon(bld) for bld in buildings]:
         x, y = poly.exterior.xy
-        ax.plot(x, y, c='royalblue', label='BGT polygon')
+        ax.plot(x, y, c=bgt_colors['pand_poly'], label='BGT polygon')
     for poly in buildings_ofs:
         x, y = poly.exterior.xy
-        ax.plot(x, y, c='royalblue', linestyle='--', label='Offset polygon')
+        ax.plot(x, y, c=bgt_colors['pand_poly'], linestyle='--',
+                label='Offset polygon')
 
     box = patches.Rectangle((x_min, y_min), x_max-x_min, y_max-y_min,
-                            linewidth=1, linestyle='--', edgecolor='darkgrey',
+                            linewidth=1, linestyle='--', edgecolor='black',
                             fill=False)
     ax.add_patch(box)
 
-    ax.set_title(f'building_{tilecode}')
+    ax.set_title(f'Buildings tile {tilecode}')
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
     ax.set_xticks(range(x_min, x_max+1, 10))
@@ -200,4 +259,8 @@ def plot_buildings_ahn_bgt(tilecode, ahn_reader, building_file, offset=1):
     ax.legend(by_label.values(), by_label.keys(),
               loc='center left', bbox_to_anchor=(1, 0.5))
 
+    padding = 2.5
+    ax.set_xlim((x_min - padding, x_max + padding))
+    ax.set_ylim((y_min - padding, y_max + padding))
+    ax.set_aspect('equal', adjustable='box')
     plt.show()
