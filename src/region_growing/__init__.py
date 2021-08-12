07722c4dbb9ad873573c2f5dbc1469cf4d124212
daanbl <daan.bloembergen@gmail.com>
2021-08-12 10:12:43 +0200

    Omit region_growing from module __init__ to prevent open3d-ml loading unnecessarily.

--- src/region_growing/__init__.py
+++ src/region_growing/__init__.py
@@ -1,5 +1,4 @@
-from .region_growing import RegionGrowing
 from .label_connected_comp import LabelConnectedComp
 from .layer_lcc import LayerLCC
 
-__all__ = ['RegionGrowing', 'LabelConnectedComp', 'LayerLCC']
+__all__ = ['LabelConnectedComp', 'LayerLCC']
