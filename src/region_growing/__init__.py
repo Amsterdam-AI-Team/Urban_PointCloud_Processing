78f8a7ae87695aa8fb8ff0c766e12e09ee19a548
daanbl <daan.bloembergen@gmail.com>
2021-12-10 16:05:27 +0100

    First draft of RefineGround module. Works for poles and trees.

--- src/region_growing/__init__.py
+++ src/region_growing/__init__.py
@@ -1,4 +1,5 @@
 from .label_connected_comp import LabelConnectedComp
 from .layer_lcc import LayerLCC
+from .refine_ground import RefineGround
 
-__all__ = ['LabelConnectedComp', 'LayerLCC']
+__all__ = ['LabelConnectedComp', 'LayerLCC', 'RefineGround']
